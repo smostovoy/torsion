ccf98b74a52dc110c23f67d273e22956f1c08c1b
sergey_mo <svmostovoy@gmail.com>
2012-01-24 08:48:44 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torsion::Application.config.secret_token = '0c57c9634408e319248e744ba1b6046064fa1225754587d9069302919f2706a9114025364b00f4581fa26ab6eb1ef6c37d55d551c9b1217d1bf401d09c3c4cdd'
